c3f1610abb79cb66c4ff6086d181bd4ebcbe77de
willmondal <wbmondal@aol.com>
2020-12-01 15:32:09 -0800

    click

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
 // Your code goes here
-const funWelcome = document.querySelector('.intro h2')
-funWelcome.addEventListener('mouseover', function(event){
+const titles = document.querySelector('h2')
+titles.addEventListener('mouseover', function(event){
     event.target.style.color = "orange"
     event.target.style.fontSize = "50px"
     setTimeout(() => {
         event.target.style.color = ""
         event.target.style.fontSize = ""
 
-    }, 1000);
+    }, 800);
 })
 
+const mainImage1 = document.querySelector('.img-content img')
+mainImage1.addEventListener('click', function(event){
+    event.target.src = 'https://upload.wikimedia.org/wikipedia/commons/e/eb/Machu_Picchu%2C_Peru.jpg'
+    // setTimeout(() => {
+    //     event.target.src = "img/adventure.jpg"
+    // }, 1500);
+})
+
+const mainImage2 = document.querySelector('.inverse-content img')
+mainImage2.addEventListener('click', function(event){
+    event.target.src = 'https://cdn.britannica.com/82/94382-050-20CF23DB/Great-Wall-of-China-Beijing.jpg'
+})
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'b'){
+        console.log('hello')
+    }
+})
