b9a7bf8b6cec403d5f014ffd96d1acad7ffb59ab
willmondal <wbmondal@aol.com>
2020-12-01 14:44:17 -0800

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const funWelcome = document.querySelector('.intro h2')
+funWelcome.addEventListener('mouseover', function(event){
+    event.target.style.color = "orange"
+    event.target.style.fontSize = "50px"
+    setTimeout(() => {
+        event.target.style.color = ""
+        event.target.style.fontSize = ""
+
+    }, 1000);
+})
+
